717e69eb3508514051b0e2c6ff9b341ab9072403
Abdul331-A <bdulamcr7@gmail.com>
2026-01-31 18:09:22 +0530

    change port

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ await connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || '192.168.220.5';
+// const HOST = process.env.HOST || '192.168.220.5';
 
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -43,6 +43,6 @@ app.get("/", (req, res) => {
 
 app.use('/api/auth', userRouter)
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
